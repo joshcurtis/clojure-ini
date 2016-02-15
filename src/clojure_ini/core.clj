6d8af269aead4ed6893de762788cae877c1a89f3
Josh Curtis <jcurtis825@gmail.com>
2016-02-15 10:36:54 -0800

    Can write INI files with multiple sections and single line comments

--- src/clojure_ini/core.clj
+++ src/clojure_ini/core.clj
@@ -1,9 +1,30 @@
-(ns clojure-ini.core)
+(ns clojure-ini.core
+  (:require [clojure.string :refer [join]]))
 
 (defn format-key-value
-  "Formats a key value pair preceded by an optional 1 line comment"
-  [[k v c]]
-  (let [key-value-str (str k "=" v)]
-    (if (some? c)
-      (str (str "; " c "\n" key-value-str))
+  "Formats a key value pair preceded by an optional 1 line comment
+  the input should be a vector of the form [key value comment]"
+  [[key value comment]]
+  (let [key-value-str (str key "=" value)]
+    (if (some? comment)
+      (str (str "# " comment "\n" key-value-str))
       key-value-str)))
+
+(defn format-section-heading
+  "Formats the heading of an ini section. The heading should be a string"
+  [title]
+  (str "[" title "]"))
+
+(defn format-section
+  "A section is a vector of key-values and an optional heading.
+  It should have the form [key-values heading]
+  Example usage: (format-section [[[key1 value1 comment1] [key2 value2]] Heading])"
+  [[key-values title]]
+  (let [key-value-strs (clojure.string/join "\n" (map format-key-value key-values))]
+    (if (some? title)
+      (str (format-section-heading title) "\n\n" key-value-strs)
+      key-value-strs)))
+
+(defn format-ini-file
+  [sections]
+  (join "\n\n\n" (map format-section sections)))
--- test/clojure_ini/core_test.clj
+++ test/clojure_ini/core_test.clj
@@ -3,6 +3,20 @@
             [clojure-ini.core :refer :all]))
 
 (deftest clojure-ini-test
-  (testing "format-key-value"
-    (is (= "; c\na=b" (format-key-value ["a" "b" "c"])))
-    (is (= "a=b" (format-key-value ["a" "b"])))))
+  (testing "Formatting"
+    (is (= "# c\na=b" (format-key-value ["a" "b" "c"])))
+    (is (= "a=b" (format-key-value ["a" "b"])))
+    (is (= "[Section Title]" (format-section-heading "Section Title")))
+    (is (= "[Heading]\n\nk=v\n# a comment\nb=v"
+           (format-section [[["k" "v"] ["b" "v" "a comment"]] "Heading"])))
+    (is (= "k=v\n# a comment\nb=v"
+           (format-section [[["k" "v"] ["b" "v" "a comment"]]])))
+    (is (= "k1=v1\nc1=v1"
+           (format-ini-file [[[["k1" "v1"] ["c1" "v1"]]]])))
+    (is (= "k1=v1\nc1=v1\n\n\nk2=v2\nc2=v2"
+           (format-ini-file [[[["k1" "v1"] ["c1" "v1"]]]
+                             [[["k2" "v2"] ["c2" "v2"]]]])))
+    (is (= "[Heading]\n\nk2=v2\nc2=v2\n\n\nk1=v1\nc1=v1"
+           (format-ini-file [[[["k2" "v2"] ["c2" "v2"]] "Heading"]
+                             [[["k1" "v1"] ["c1" "v1"]]]])))
+    ))
